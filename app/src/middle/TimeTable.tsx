6bf8f444fd9dc67bd9fe434ce12463b185fed9b3
伊藤　秀平 <75195951+I-2ki@users.noreply.github.com>
2024-01-25 04:53:56 +0900

    無駄なimportを削除

--- app/src/middle/TimeTable.tsx
+++ app/src/middle/TimeTable.tsx
@@ -5,7 +5,6 @@ import NameCard from "./NameCard";
 
 import { displayStudent } from "../firebase/db/displayStudent";
 import { explaingDay } from "./ToolBer/DaySelector";
-import { cellCount } from "../logic/timeZone";
 
 function TimeTable() {
 	const container = css`
